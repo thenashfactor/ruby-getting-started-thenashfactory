598cc1e846a3981424de31aa28f18c5c148932f0
Philip Nash <pwnash@gmail.com>
2015-11-28 17:02:03 -0800

    Patch commits from original clone

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -8,6 +8,9 @@ Rails.application.configure do
 
   # Do not eager load code on boot.
   config.eager_load = false
+  config.action_dispatch.default_headers = {
+        'X-Frame-Options' => 'ALLOWALL'
+          }
 
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,7 @@
 Rails.application.configure do
+  config.action_dispatch.default_headers = {
+        'X-Frame-Options' => 'ALLOWALL'
+          }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
